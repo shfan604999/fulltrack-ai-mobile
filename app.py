6ee09ccfc3efdb921964e4a8dbf3d6e7497b315e
shfan604999 <shfan604@gmail.com>
2026-02-04 13:25:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
-import numpy as np
 import matplotlib.pyplot as plt
 import tempfile
 from ball_tracker import track_ball
+from speed_calc import calculate_speed
 
 st.set_page_config(page_title="Cricket AI")
 
-st.title("🏏 Cricket Ball Tracker (AI)")
+st.title("🏏 Cricket Ball Speed Analyzer")
 
-video = st.file_uploader("Upload cricket bowling video", type=["mp4"])
+video = st.file_uploader("Upload bowling video", type=["mp4"])
 
 if video:
     with tempfile.NamedTemporaryFile(delete=False) as tmp:
@@ -16,11 +16,13 @@ if video:
         video_path = tmp.name
 
     st.video(video_path)
-    st.info("⏳ AI is analyzing the video...")
+    st.info("⏳ AI analyzing ball...")
 
     positions = track_ball(video_path)
 
     if positions:
+        speed = calculate_speed(positions)
+
         x = [p[0] for p in positions]
         y = [p[1] for p in positions]
 
@@ -30,6 +32,6 @@ if video:
         ax.set_title("Ball Trajectory")
         st.pyplot(fig)
 
-        st.success("✅ Ball detected successfully")
+        st.success(f"🔥 Ball Speed: {speed} km/h")
     else:
-        st.error("❌ Ball not detected. Try clear video.")
+        st.error("❌ Ball not detected. Use clear slow-motion video.")
